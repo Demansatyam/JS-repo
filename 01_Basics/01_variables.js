1f7a788902a79e6da2f3e59017158320cdcfda90
crimeXprime <kumarsatyam01303@gmail.com>
2024-01-30 23:51:14 +0530

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14812
+let accountEmail = "kumarsatyam01303@gmail.com"
+var accountPassword = "Baymax@1234"
+accountCity = "Ludhiana"
+
+// let and var variables can be change but const variable cannot
+// accountEmail = "kumarsatyam15963@gmail.com"
+// accountPassword = "hello@12345"
+// accountCity = "Darbhanga"
+
+console.log(accountId , accountEmail, accountPassword, accountCity);
+console.table([accountId , accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+// Treat all your js code as newer version
+"use strict"; 
+
+let name1 = "Satyam"
+let age = 18
+
+// we are using nodejs not browser
+// alert(3+3)
+
+// Following are the primitive datatypes:
+// number => range upto 2 to the power 53
+// BigInt => to store number more than 2 to the power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+
+// Non-primitve datatypes:
+// object
+
+console.log(typeof name1)
+console.log(typeof age);
+
+// type of null is object
+console.log(typeof null);
+// type of undefined is undefined
+console.log(typeof undefined);
